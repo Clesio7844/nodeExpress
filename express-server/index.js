ed242c528408592c0c47280413cfeea8dd1250d9
David De Castro <daviddecastro42@live.co.uk>
2019-11-12 11:01:03 +0000

    first commit

---
+++ express-server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// app.get('/', function(req, res){
+//  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(3000, () =>{
+    console.log('server listning on port 3000');
+});
